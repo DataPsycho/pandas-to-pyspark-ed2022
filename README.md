ef3bc855dab3aa34b3b5bd1a2ebb59cae8688cbd
DataPsycho <mr.data.psycho@gmail.com>
2022-01-23 09:37:21 +0100

    Adding Readme and catalog from data directory

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pandas to PySpark DataFrame: Edition 2022
+
+The repository contains source code of Pandas to PySpark book. The book can be purchased from [learnpub](https://leanpub.com/pandas-to-pyspark).
+
+![](images/pandas-to-pyspark-cover.png)
---
+++ data/raw/datadownloader.sh
@@ -0,0 +1,4 @@
+wget http://deepyeti.ucsd.edu/jianmo/amazon/sample/sample_Home_and_Kitchen_5.json
+wget http://deepyeti.ucsd.edu/jianmo/amazon/sample/sample_meta_Home_and_Kitchen.json
+wget http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/Toys_and_Games_5.json.gz
+
